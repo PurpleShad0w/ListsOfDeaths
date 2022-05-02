5546d98747d7a8babcf3051dcc04f6960ad3c59b
Octave <thepurpl3shad0w@gmail.com>
2022-05-03 01:19:45 +0200

    first commit

---
+++ death.py
@@ -0,0 +1,70 @@
+import os
+import sys
+from matplotlib.pyplot import axis
+import pandas as pd
+import warnings
+import networkx as nx
+from pyvis.network import Network
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+
+def murder_graph(df):
+    # Classify the data
+    sources, targets, status = df['Killer'], df['Victim'], df['Status']
+
+    # Create an empty network
+    net = Network(height="100%", width="100%", bgcolor="#111111", font_color="pink", directed=True)
+
+    # Compile edge data
+    edge_data = zip(sources, targets, status)
+
+    # Create nodes and edges
+    for src, dst, status in edge_data:
+        # Change color of nodes based on status
+        if status == 'Alive':
+            color = 'green'
+        if status == 'Deceased':
+            color = 'crimson'
+        new_dst = dst
+        # Check for numbered characters
+        if any(char.isdigit() for char in dst):
+            number = [char for char in dst if char.isdigit()]
+            number = int(''.join(map(str,number)))
+            if number == 1:
+                new_dst = str(number)+' person'
+            else:
+                new_dst = str(number)+' people'
+        # Add nodes and edges
+        net.add_node(src, src, title=src, color=color)
+        net.add_node(dst, new_dst, title=dst, color='crimson')
+        net.add_edge(src, dst)
+    
+    # Output the graph
+    return(net)
+
+
+def kill_count(df):
+    # Initiate dataframe
+    kills = pd.DataFrame({'Killer':0,'Kills':0},index=(0,1))
+
+    # Attribute kills to each killer
+    for i in range(len(df)):
+        victim = df.loc[i,'Victim']
+        if any(char.isdigit() for char in victim):
+            number = [char for char in victim if char.isdigit()]
+            number = int(''.join(map(str,number)))
+        else:
+            number = 1
+        s = {'Killer':df.loc[i,'Killer'],'Kills':number}
+        kills = kills.append(s,ignore_index=True)
+    
+    # Clean and sort dataframe
+    kills = kills[kills['Kills'] != 0]
+    kills = kills.groupby('Killer').sum()
+    kills = kills.sort_values(by='Kills',ascending=False)
+
+    # Output the data
+    return(kills)
---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import pandas as pd
+import warnings
+from death import murder_graph
+from death import kill_count
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+os.chdir(os.path.dirname(sys.argv[0]))
+
+
+# Load Attack on Titan data
+aot = pd.read_csv('source/attack_on_titan.csv')
+aot_filtered = aot[(aot['Type'] == 'Individual') | (aot['Type'] == 'Group')]
+
+# Create graph and kill count
+murder_graph(aot_filtered).write_html('graphs/aot_graph.html')
+kill_count(aot).to_csv('kill_counts/aot_kills.csv')
