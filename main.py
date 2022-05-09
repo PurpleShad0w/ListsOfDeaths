8c94b9914a202f544ea804b1007910ac4a6f126f
Octave <thepurpl3shad0w@gmail.com>
2022-05-09 03:46:00 +0200

    Update

--- main.py
+++ main.py
@@ -12,12 +12,11 @@ os.chdir(os.path.dirname(sys.argv[0]))
 # Create the required folders
 if not os.path.isdir('graphs'):
     os.mkdir('graphs')
-    os.mkdir('kill_counts')
 
 # Load Attack on Titan data
-aot = pd.read_csv('source/attack_on_titan.csv')
+aot = pd.read_csv('death_lists/attack_on_titan.csv')
 aot_filtered = aot[(aot['Type'] == 'Individual') | (aot['Type'] == 'Group')]
 
 # Create graph and kill count
-murder_graph(aot_filtered).write_html('graphs/aot_graph.html')
-kill_count(aot).to_csv('kill_counts/aot_kills.csv')
+murder_graph(aot_filtered).write_html('graphs/aot_who_killed_who_graph.html')
+kill_count(aot).to_csv('kill_counts/aot_top_killers.csv')
