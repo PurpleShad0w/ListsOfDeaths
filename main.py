7ba5c1181df72011195aeac9d2fa84cc14a34c3f
Octave <thepurpl3shad0w@gmail.com>
2022-05-11 18:33:51 +0200

    Added jojo part 1-4

--- main.py
+++ main.py
@@ -13,10 +13,16 @@ os.chdir(os.path.dirname(sys.argv[0]))
 if not os.path.isdir('graphs'):
     os.mkdir('graphs')
 
-# Load Attack on Titan data
+# Load data
 aot = pd.read_csv('death_lists/attack_on_titan.csv')
 aot_filtered = aot[(aot['Type'] == 'Individual') | (aot['Type'] == 'Group')]
+jojo = pd.read_csv('death_lists/jojo.csv')
+jojo_filtered = jojo[(jojo['Type'] == 'Individual') | (jojo['Type'] == 'Group')]
 
-# Create graph and kill count
+# Create graphs
 murder_graph(aot_filtered).write_html('graphs/aot_who_killed_who_graph.html')
+murder_graph(jojo_filtered).write_html('graphs/jojo_who_killed_who_graph.html')
+
+# Create kill counts
 kill_count(aot).to_csv('kill_counts/aot_top_killers.csv')
+kill_count(jojo).to_csv('kill_counts/jojo_top_killers.csv')
